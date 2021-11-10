fa8b777960dbf8b2d1a2d4d8b8b09523d46973b8
phamhung01 <94037928+phamhung01@users.noreply.github.com>
2021-11-10 14:10:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const nameGirl = 'Em';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-0';
+const eventName = 'Anh yêu em nhiều lắm';
 const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const contentCard = 'Chúc bé Linh của anh một ngày tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
